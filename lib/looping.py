a7f4bd183934a871a09b21d3e7da80df209768e4
jim <jim.otieno@student.moringaschool.com>
2023-11-21 23:38:03 +0300

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,35 @@
 
 def happy_new_year():
     # code goes here!
+    counter=10
+    while counter>0:
+       print(counter)
+       counter -= 1
+    print("Happy New Year!")
     pass
+happy_new_year()
 
 def square_integers(int_list):
+    height_incrime=[int*int for int in int_list]
+    return height_incrime
     # code goes here!
     pass
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):  # Range from 1 to 100 inclusive
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+# Call the function to print FizzBuzz
+fizzbuzz()
+
+
+
+    
+
